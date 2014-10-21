9cc2bd4d759421ab4b963822e3a24f2e400be380
Gianni Chiappetta <gianni@runlevel6.org>
2014-10-21 11:44:14 -0400

    Use the web 2.O in this version

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source :rubygems
-
-gem "middleman"
-gem "redcarpet", ">= 2.1.1"
-gem "pygments.rb", ">= 0.2.12"
-
--- source/read/2012-11-07-exception-introspection/attempt-2.m
+++
@@ -1,7 +0,0 @@
-- (void)webView:(WebView *)webView exceptionWasRaised:(WebScriptCallFrame *)frame hasHandler:(BOOL)hasHandler sourceId:(WebSourceId)sid line:(int)lineno forWebFrame:(WebFrame *)webFrame
-{
-  [[webFrame windowObject] setValue:[frame exception] forKey:@"__GC_frame_exception"];
-  id objectRef = [[webFrame windowObject] evaluateWebScript:@"__GC_frame_exception.constructor.name"];
-  [[webFrame windowObject] setValue:nil forKey:@"__GC_frame_exception"];
-}
-
